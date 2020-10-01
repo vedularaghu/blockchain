c530071f45ab3831b4fa151f5fc301d13cdea55f
Asutosh <asutoshpanda989@gmail.com>
2020-10-01 19:31:09 +0530

    refactor: Update bcrypt package and other packages making this compatible with node v12.13.0

--- routes/index.js
+++ routes/index.js
@@ -7,54 +7,54 @@ const birth_st = require('../models/birth');
 
 var router = express.Router();
 const user = 'admin';
-const pass = '$2a$10$uL09gFqwJpk9q7OU2j9K8.mq011rgYqUSJvnQfLw2cIWuTeoatiYm';
-const saltRounds = 10;
-const aadhar_list= [];
+const pass = '$2a$04$Q.x1kX.BSh4PQ2rkM2h4AOtwO3vX.FlY4Pb4rQ.E0WWmnTxR0nKtC';
+const saltRounds = 4;
+const aadhar_list = [];
 const birth_list = [];
 // router.use(session({secret: "Your secret key"}));
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index',{message:null});
+router.get('/', function (req, res) {
+  res.render('index', { message: null });
 });
 
-function isAuthenticated(req,res,next) {
+function isAuthenticated(req, res, next) {
   console.log(req.session.user);
   if (!req.session.user) {
-      res.send('You are not authorized to view this page');
-      console.log(req.session);
-      console.log(req.session.cookie.maxAge);
-    } else {
-      next();
-    }
+    res.send('You are not authorized to view this page');
+    console.log(req.session);
+    console.log(req.session.cookie.maxAge);
+  } else {
+    next();
+  }
 }
-router.post('/login', function(req, res, next) {
+router.post('/login', function (req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
   if (username == user) {
-    bcrypt.compare(password, pass).then((result)=> {
-      if (result){
+    bcrypt.compare(password, pass).then((result) => {
+      if (result) {
         req.session.user = username;
         req.session.save();
         // console.log(req.body);
         res.redirect('/dashboard');
       }
       else {
-        res.render('index', {message:"error"});
+        res.render('index', { message: "error" });
       }
-    }).catch((err)=> {next(err);});
+    }).catch((err) => { next(err); });
   }
   else {
     // res.send("Bad Username or Password");
-    res.render('index', {message:"error"});
+    res.render('index', { message: "error" });
   }
 });
 
-router.post('/aadhar', function(req, res, next) {
+router.post('/aadhar', function (req, res, next) {
   var name = req.body.nameaadhar;
   var an = req.body.aadharnum;
-  var tohash = name+an;
+  var tohash = name + an;
   console.log(tohash);
-  bcrypt.hash(tohash,saltRounds, function(err, hash) {
+  bcrypt.hash(tohash, saltRounds, function (err, hash) {
     res.render('result');
     console.log(hash);
     const A = new aadhar_st({
@@ -63,23 +63,23 @@ router.post('/aadhar', function(req, res, next) {
       unique_hash: hash,
     });
     A.save().then(function (err, aadhar_st) {
-      if(err){
+      if (err) {
         console.log(err);
         res.render(err);
-      }else{
+      } else {
         res.redirect('/aadhar?msg=Successfully%20saved')
       }
     })
-    aadhar_list.push({name:name,aadhar_hash: hash});
-});
+    aadhar_list.push({ name: name, aadhar_hash: hash });
+  });
 
 });
-router.post('/birth', function(req, res, next) {
+router.post('/birth', function (req, res, next) {
   var name = req.body.namebirth;
   var birthdate = req.body.date;
-  var tohash = name+birthdate;
+  var tohash = name + birthdate;
   console.log(tohash);
-  bcrypt.hash(tohash, saltRounds, function(err, hash) {
+  bcrypt.hash(tohash, saltRounds, function (err, hash) {
     res.render('result');
     console.log(hash);
     const B = new aadhar_st({
@@ -88,28 +88,28 @@ router.post('/birth', function(req, res, next) {
       unique_hash: hash,
     });
     B.save().then(function (err, aadhar_st) {
-      if(err){
+      if (err) {
         console.log(err);
         res.send(err);
-      }else{
+      } else {
         res.redirect('/birth?msg=Successfully%20Saved');
       }
     })
-    birth_list.push({name:name, birth_hash:hash})
-});
+    birth_list.push({ name: name, birth_hash: hash })
+  });
 
 });
 
-router.get('/dashboard', isAuthenticated, function(req, res) {
+router.get('/dashboard', isAuthenticated, function (req, res) {
   res.render('dashboard');
 });
-router.get('/my-page', isAuthenticated, function(req,res) {
+router.get('/my-page', isAuthenticated, function (req, res) {
   res.render('page');
 });
-router.get('/register-birth', isAuthenticated, function(req,res) {
+router.get('/register-birth', isAuthenticated, function (req, res) {
   res.render('register1');
 });
-router.get('/register-aadhar', isAuthenticated, function(req,res) {
+router.get('/register-aadhar', isAuthenticated, function (req, res) {
   res.render('register2');
 });
 
